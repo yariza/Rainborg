bcc44fdcf434ac140bb01625f49906e98d8b7df3
errorbound <bound.for.error@gmail.com>
2015-04-11 18:39:40 -0400

    Empty starting files

---
+++ src/Simulator.h
@@ -0,0 +1,7 @@
+
+
+// Scene scene
+
+// Stepper stepper
+
+// Renderer render
---
+++ src/testFile.cpp
@@ -0,0 +1,91 @@
+#include <GLFW/glfw3.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <glm/glm.hpp>
+#include "rapidxml.hpp"
+#include <tclap/CmdLine.h>
+
+static void error_callback(int error, const char* description)
+{
+    fputs(description, stderr);
+}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+int foo()
+{
+        glm::vec4 Position = glm::vec4(glm::vec3(0.0), 1.0);
+        glm::mat4 Model = glm::mat4(1.0);
+        Model[3] = glm::vec4(1.0, 1.0, 0.0, 1.0);
+        glm::vec4 Transformed = Model * Position;
+        return 0;
+}
+
+int main(void)
+{
+    std::cout << foo() << std::endl;
+
+    GLFWwindow* window;
+
+    glfwSetErrorCallback(error_callback);
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    glfwSwapInterval(1);
+    glfwSetKeyCallback(window, key_callback);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        float ratio;
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float) height;
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+        glBegin(GL_TRIANGLES);
+            glColor3f(1.f, 0.f, 0.f);
+            glVertex3f(-0.6f, -0.4f, 0.f);
+            glColor3f(0.f, 1.f, 0.f);
+            glVertex3f(0.6f, -0.4f, 0.f);
+            glColor3f(0.f, 0.f, 1.f);
+            glVertex3f(0.f, 0.6f, 0.f);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
+
