0718cdcba7185d2e1079161a52c5cca0b4e9e32f
errorbound <bound.for.error@gmail.com>
2015-04-18 16:36:09 -0400

    Kernellllssss

--- src/MathDefs.cpp
+++ src/MathDefs.cpp
@@ -11,22 +11,43 @@ scalar wPoly6Kernel(Vector3s &pi, Vector3s &pj, scalar h){
         return 0; 
 
     r = ((h * h) - (r * r)); 
-    r = r * r * r; 
-    return r * (315.0 / (64.0 * PI * h * h * h * h));
+    r = r * r * r; // (h^2 - r^2)^3
+    return r * (315.0 / (64.0 * PI * h * h * h * h * h * h * h * h * h));
 }
 
 
 scalar wSpikyKernel(Vector3s &pi, Vector3s &pj, scalar h){
-return 0; 
+    scalar r = glm::distance(pi, pj); 
+    if(r > h || r < 0)
+        return 0;
+    
+    r = (h - r); 
+    r = r * r * r;  // (h-r)^3
+    return r * (15.0 / (PI * h * h * h * h * h * h)); 
 }
 
-Vector3s wPoly6KernelGrad(Vector3s &pi, Vector3s &pj, scalar h){
+scalar wViscosityKernel(Vector3s &pi, Vector3s &pj, scalar h){
+    scalar r = glm::distance(pi, pj); 
+    if(r > h || r < 0)
+        return 0;
+    
+    scalar num = -(r*r*r)/(2.0*h*h*h) + (r*r)/(h*h) + h/(2.0*r) - 1; 
+    return num * (15.0 / (2.0 * PI * h * h * h)); 
+        
+}
 
+
+Vector3s wPoly6KernelGrad(Vector3s &pi, Vector3s &pj, scalar h){
 
 }
 
 Vector3s wSpikyKernelGrad(Vector3s &pi, Vector3s &pj, scalar h){
-
+    Vector3s dp = pi - pj; 
+    scalar r = glm::length(dp);  
+    if(r > h || r < 0)
+        return Vector3s(0.0, 0.0, 0.0); 
+    scalar scale = -45.0 / (PI * h * h * h * h * h * h) * (h - r) * (h - r); 
+    return scale * dp; 
 }
 
 
