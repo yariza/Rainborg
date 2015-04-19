bd03abfa229a6461198ec6054eaa8436fd594917
Yujin Ariza <yujin.ariza@gmail.com>
2015-04-19 01:04:50 -0400

    copy operations for cmake

--- cmake/Common.cmake
+++ cmake/Common.cmake
@@ -11,3 +11,28 @@ macro (append_files files ext)
     list (APPEND ${files} ${_files})
   endforeach (dir)
 endmacro (append_files)
+
+macro(copy_files TGT_NAME GLOBPAT DESTINATION)
+  file(GLOB COPY_FILES
+    RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}
+    ${GLOBPAT})
+
+  foreach(FILENAME ${COPY_FILES})
+    get_filename_component(BASENAME ${FILENAME} NAME)
+    set(SRC "${CMAKE_CURRENT_SOURCE_DIR}/${FILENAME}")
+    set(DST "${DESTINATION}/${BASENAME}")
+
+    add_custom_command(
+      OUTPUT ${DST}
+      COMMAND ${CMAKE_COMMAND} -E copy ${SRC} ${DST}
+      COMMENT "Copying ${SRC} to ${DST}"
+      MAIN_DEPENDENCY ${SRC}
+      )
+    set(COPY_TGT_NAME "copy.${TGT_NAME}.${BASENAME}")
+    add_custom_target(
+        ${COPY_TGT_NAME}
+        DEPENDS ${DST}
+    )
+    add_dependencies(${TGT_NAME} ${COPY_TGT_NAME})
+  endforeach(FILENAME)
+endmacro(copy_files)
