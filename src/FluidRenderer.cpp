e7aebe860329db79498c936ca1427ca92fa57264
Yujin Ariza <yujin.ariza@gmail.com>
2015-04-24 03:38:43 -0400

    preparation for adding gpu positions

--- src/FluidRenderer.cpp
+++ src/FluidRenderer.cpp
@@ -12,36 +12,40 @@ FluidRenderer::FluidRenderer(Fluid* fluid)
 {
     assert(m_fluid != NULL);
 
-    vertices = new GLfloat[4 * NUM_PARTS];
-    indices = new GLuint[NUM_PARTS];
+    if (g_gpu_mode) {
+        vertices = new GLfloat[4 * NUM_PARTS];
+        indices = new GLuint[NUM_PARTS];
 
-    GLfloat x, y, z;
-    for (int i=0; i<NUM_PARTS; i++) {
-        x = static_cast <GLfloat> (rand()) / static_cast<GLfloat>(RAND_MAX/9.0);
-        y = static_cast <GLfloat> (rand()) / static_cast<GLfloat>(RAND_MAX/9.0);
-        z = static_cast <GLfloat> (rand()) / static_cast<GLfloat>(RAND_MAX/9.0);
+        GLfloat x, y, z;
+        for (int i=0; i<NUM_PARTS; i++) {
+            x = static_cast <GLfloat> (rand()) / static_cast<GLfloat>(RAND_MAX/9.0);
+            y = static_cast <GLfloat> (rand()) / static_cast<GLfloat>(RAND_MAX/9.0);
+            z = static_cast <GLfloat> (rand()) / static_cast<GLfloat>(RAND_MAX/9.0);
 
-        vertices[i*4 + 0] = x;
-        vertices[i*4 + 1] = y;
-        vertices[i*4 + 2] = z;
-        vertices[i*4 + 3] = 1.0f;
+            vertices[i*4 + 0] = x;
+            vertices[i*4 + 1] = y;
+            vertices[i*4 + 2] = z;
+            vertices[i*4 + 3] = 1.0f;
 
-        indices[i] = i;
+            indices[i] = i;
+        }
+
+        glGenBuffers(1, &vbo);
+        glGenBuffers(1, &ibo);
+
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, 4*NUM_PARTS*sizeof(GLfloat), vertices, GL_DYNAMIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, NUM_PARTS*sizeof(GLuint), indices, GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+        #ifdef GPU_ENABLED
+        cudaGLSetGLDevice( compat_getMaxGflopsDeviceId() );
+        cudaGLRegisterBufferObject(vbo);
+        #endif
     }
-
-    glGenBuffers(1, &vbo);
-    glGenBuffers(1, &ibo);
-
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, 4*NUM_PARTS*sizeof(GLfloat), vertices, GL_DYNAMIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, NUM_PARTS*sizeof(GLuint), indices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-    // cudaGLSetGLDevice( compat_getMaxGflopsDeviceId() );
-    // cudaGLRegisterBufferObject(vbo);
 }
 
 FluidRenderer::~FluidRenderer() {
@@ -66,8 +70,16 @@ void FluidRenderer::render(GLFWViewer* viewer, int width, int height) {
     m_shader.setMatrix4x4Uniform("projectionMatrix", camera.getProjectionMatrix());
 
     glPointSize(5.0);
+    glEnable(GL_POINT_SMOOTH);
 
     if (g_gpu_mode) {
+
+        float *dptrvert=NULL;
+        #ifdef GPU_ENABLED
+        cudaGLMapBufferObject((void**)&dptrvert, vbo);
+        // updateVBO(dptrvert); // update content inside vbo object - implement this method in kernel!
+        cudaGLUnmapBufferObject(vbo);
+        #endif
 
         glEnableVertexAttribArray(position_location);
 
@@ -84,7 +96,6 @@ void FluidRenderer::render(GLFWViewer* viewer, int width, int height) {
 
         Vector3s* posArray = m_fluid->getFPPos();
 
-        glEnable(GL_POINT_SMOOTH);
         glBegin(GL_POINTS);
             for (int i=0; i<m_fluid->getNumParticles(); i++) {
                 glVertex3f(posArray[i].x, posArray[i].y, posArray[i].z);
